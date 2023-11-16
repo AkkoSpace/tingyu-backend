c128fa63c541b44f2023fb686aa1038a3a9c050b
Akko <akkoyan@qq.com>
2023-11-16 21:19:13 +0800

    Initial commit

---
+++ src/main/java/space/akko/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package space.akko.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import space.akko.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
